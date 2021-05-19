094d334b94c710b1d5f9d210542336471a8983c8
Adriana Diniz <dri.ragazoni@gmail.com>
2021-05-19 11:54:42 -0300

    teste

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 @Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
-
+//classe de negocio
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
